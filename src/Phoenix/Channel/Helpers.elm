5885ac8a6999b81feb94bf6bb0e5e0bfc9eba346
C. R. Oldham <cro@ncbt.org>
2017-09-04 13:23:27 -0600

    upgrade to 0.18

--- src/Phoenix/Channel/Helpers.elm
+++ src/Phoenix/Channel/Helpers.elm
@@ -1,10 +1,14 @@
-module Phoenix.Channel.Helpers exposing (assignResponseType) -- where
+module Phoenix.Channel.Helpers exposing (assignResponseType)
+
 {-| Helpers
 
 Convert your response type from a string to a message
 @docs assignResponseType
 
 -}
+
+-- where
+
 import Json.Decode exposing (decodeString)
 import Phoenix.Channel.Model exposing (decodeSocketMessage, SocketMessage)
 import Phoenix.Channel.Update exposing (Msg(..))
@@ -12,19 +16,22 @@ import Phoenix.Channel.Update exposing (Msg(..))
 
 turnDecodedResponseIntoServerResponse : Result String SocketMessage -> Msg
 turnDecodedResponseIntoServerResponse response =
-  case response of
-    Ok socketResponse ->
-      case socketResponse.payload.status of
-        "ok" ->
-          SuccessfulResponse socketResponse
-        _ ->
-          ErrorResponse (socketResponse.payload.status)
-    Err message ->
-      ErrorResponse message
+    case response of
+        Ok socketResponse ->
+            case socketResponse.payload.status of
+                "ok" ->
+                    SuccessfulResponse socketResponse
+
+                _ ->
+                    ErrorResponse (socketResponse.payload.status)
+
+        Err message ->
+            ErrorResponse message
+
 
 {-| Converts a string into a Msg
 -}
 assignResponseType : String -> Msg
 assignResponseType =
     decodeString decodeSocketMessage
-      >> turnDecodedResponseIntoServerResponse
+        >> turnDecodedResponseIntoServerResponse
--- src/Phoenix/Channel/Model.elm
+++ src/Phoenix/Channel/Model.elm
@@ -1,7 +1,13 @@
-module Phoenix.Channel.Model exposing (
-  Response, ResponsePayload, SocketMessage, MessageToSend
-  , encodeMessageToSend, decodeSocketMessage
-  , Model) -- where
+module Phoenix.Channel.Model
+    exposing
+        ( Response
+        , ResponsePayload
+        , SocketMessage
+        , MessageToSend
+        , encodeMessageToSend
+        , decodeSocketMessage
+        , Model
+        )
 
 {-| Types used for representing things that come from a phoenix channel
 
@@ -15,92 +21,104 @@ Dealing with server responses
 Sending stuff to the server
 @docs encodeMessageToSend
 @docs MessageToSend
+
 -}
+
+-- where
 
 import Json.Encode
 import Json.Decode exposing (..)
 import Json.Decode.Pipeline as Pipeline
 
+
 {-| A response from the server
 -}
 type alias Response =
-  { reason : String }
+    { reason : String }
+
 
 {-| The status of the response paired with the response itself
 -}
 type alias ResponsePayload =
-  { status : String
-  , response : Response
-  }
+    { status : String
+    , response : Response
+    }
+
 
 {-| Each socket response has the topic (room) as a string, along with a ref count
-    the event to trigger, and the payload of what the server actually sent
+the event to trigger, and the payload of what the server actually sent
 -}
 type alias SocketMessage =
-  { topic : String
-  , ref : Int
-  , payload : ResponsePayload
-  , event : String
-  }
+    { topic : String
+    , ref : Int
+    , payload : ResponsePayload
+    , event : String
+    }
+
 
 {-| Like a socket message, but comes with a pre-encoded payload
 -}
 type alias MessageToSend =
-  { topic : String
-  , ref : Int
-  , payload : Json.Encode.Value
-  , event : String
-  }
+    { topic : String
+    , ref : Int
+    , payload : Json.Encode.Value
+    , event : String
+    }
+
 
 {-| We demand that any model must store the socket events, the current refNumber, and
 the state of connection
 -}
 type alias Model a =
-  { a
-  | socketEvents : List SocketMessage
-  , refNumber : Int
-  , connected : Bool
-  , socketUrl : String
-  }
+    { a
+        | socketEvents : List SocketMessage
+        , refNumber : Int
+        , connected : Bool
+        , socketUrl : String
+    }
 
 
 maybeNull : Decoder a -> Decoder (Maybe a)
 maybeNull decoder =
-  Json.Decode.oneOf [ Json.Decode.null Nothing, Json.Decode.map Just decoder ]
+    Json.Decode.oneOf [ Json.Decode.null Nothing, Json.Decode.map Just decoder ]
+
 
 decodeRef : Decoder Int
 decodeRef =
-  Json.Decode.map (Maybe.withDefault -1) (maybeNull Json.Decode.int)
+    Json.Decode.map (Maybe.withDefault -1) (maybeNull Json.Decode.int)
+
 
 decodeResponse : Decoder Response
 decodeResponse =
-  Pipeline.decode Response
-    |> Pipeline.optional "reason" (string) "NULL"
+    Pipeline.decode Response
+        |> Pipeline.optional "reason" (string) "NULL"
+
 
 decodeResponsePayload : Decoder ResponsePayload
 decodeResponsePayload =
-  Pipeline.decode ResponsePayload
-    |> Pipeline.required "status" (string)
-    |> Pipeline.optional "response" decodeResponse ({ reason = ""})
+    Pipeline.decode ResponsePayload
+        |> Pipeline.required "status" (string)
+        |> Pipeline.optional "response" decodeResponse ({ reason = "" })
 
 
 {-| Decode a socket response.
 -}
 decodeSocketMessage : Decoder SocketMessage
 decodeSocketMessage =
-  Pipeline.decode SocketMessage
-    |> Pipeline.required "topic" (string)
-    |> Pipeline.optional "ref" decodeRef (-1)
-    |> Pipeline.required "payload" (decodeResponsePayload)
-    |> Pipeline.required "event" (string)
+    Pipeline.decode SocketMessage
+        |> Pipeline.required "topic" (string)
+        |> Pipeline.optional "ref" decodeRef (-1)
+        |> Pipeline.required "payload" (decodeResponsePayload)
+        |> Pipeline.required "event" (string)
+
 
 {-| Encode a message to send
 -}
 encodeMessageToSend : MessageToSend -> Json.Encode.Value
 encodeMessageToSend message =
-  Json.Encode.object
-    [ ("topic", Json.Encode.string message.topic)
-    , ("ref", Json.Encode.int message.ref)
-    , ("payload", message.payload)
-    , ("event", Json.Encode.string message.event)
-    ]
+    Json.Encode.object
+        [ ( "topic", Json.Encode.string message.topic )
+        , ( "ref", Json.Encode.int message.ref )
+        , ( "payload", message.payload )
+        , ( "event", Json.Encode.string message.event )
+        ]
